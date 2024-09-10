af333c2ecfede270cfdc273ecec7b45409a9b47d
Leonardo Marquine <leonardomarquine@gmail.com>
2024-09-09 21:42:08 -0300

    Adiciona conexão

---
+++ src/database/connection.ts
@@ -0,0 +1,13 @@
+import postgres from 'postgres';
+
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } = process.env;
+
+const sql = postgres({
+	host: POSTGRES_HOST,
+	port: parseInt(POSTGRES_PORT ?? ''),
+	database: POSTGRES_DB,
+	username: POSTGRES_USER,
+	password: POSTGRES_PASSWORD
+});
+
+export default sql;
